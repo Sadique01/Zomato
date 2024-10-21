3ea2c89375d869155c7a12c87b8830cd0fca36ff
Sadique <sadiquea@shriffle.com>
2024-10-21 15:45:44 +0530

    update production port number

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '51.20.239.21', port: 80, user: 'deploy', roles: %w{web app db}, primary: true
+server '51.20.239.21', port: 22, user: 'deploy', roles: %w{web app db}, primary: true
 set :production
 set :branch, 'master'
 set :repo_url, "git@github.com:Sadique01/Zomato.git"
