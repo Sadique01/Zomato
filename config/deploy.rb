b0ef5507c0d02c437446ef0ec222616e863771dd
Sadique <sadiquea@shriffle.com>
2024-10-21 17:31:26 +0530

    change deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,10 +4,11 @@ lock "~> 3.19.1"
 set :application, "zomato"
 set :repo_url, "git@github.com:Sadique01/Zomato.git"
 set :user,    'deploy'
- 
+
 set :branch, :master
-set :deploy_to, '/home/deploy/apps/zomato'
+set :deploy_to, '/home/deploy/zomato'
 set :pty, true
+set :use_sudo, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
@@ -17,7 +18,7 @@ set :rvm_ruby_version, 'ruby-3.2.2' # Edit this if you are using MRI Ruby
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"    #accept array for multi-bind
 set :puma_conf, "#{shared_path}/puma.rb"
 set :puma_access_log, "#{shared_path}/log/puma_error.log"
 set :puma_error_log, "#{shared_path}/log/puma_access.log" 
@@ -28,3 +29,18 @@ set :puma_workers, 0
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true
 set :puma_preload_app, true
+set :tmp_dir, "/home/#{fetch(:user)}/#{fetch(:application)}/tmp"
+
+
+namespace :puma do
+    desc 'Create Directories for Puma Pids and Socket'
+    task :make_dirs do
+      on roles(:app) do
+        execute "mkdir #{shared_path}/tmp/sockets -p"
+        execute "mkdir #{shared_path}/tmp/pids -p"
+  
+        Rake::Task['puma:start'].clear_actions
+      end
+    end
+    before :start, :make_dirs
+  end
